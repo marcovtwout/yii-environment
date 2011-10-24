82861fb58e333640dea4b78ab7ef40d2dc56ae15
marcovtwout@gmail.com <marcovtwout@gmail.com@71ca97a8-53c9-4e40-1942-0704d233fefd>
2011-10-24 08:05:44 +0000

    FIX: config->configWeb in example file

--- example-config/main.php
+++ example-config/main.php
@@ -24,7 +24,7 @@ return array(
 
 	// This is the main Web application configuration. Any writable
 	// CWebApplication properties can be configured here.
-	'config' => array(
+	'configWeb' => array(
 
 		'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 		'name' => 'My Web Application',
