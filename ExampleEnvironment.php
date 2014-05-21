9c17764cbdf543076ade3fbc785a621745fbb19b
marcovtwout <mr@vtwout.net>
2014-05-21 17:48:44 +0200

    Cleanup ExampleEnvironment file

--- ExampleEnvironment.php
+++ ExampleEnvironment.php
@@ -2,7 +2,7 @@
 
 /**
  * This is an example Environment, for when you want to use a custom $envVar
- * or $configDir, or want to use other than the predefined modes.
+ * or want to use other than the predefined modes.
  *
  * If you use the extended class, don't forget to modify your bootstrap file as well
  * to call this class.
@@ -14,16 +14,6 @@ class ExampleEnvironment extends \marcovtwout\YiiEnvironment\Environment
      * @var string name of env var to check
      */
     protected $envVar = 'MY_ENVIRONMENT';
-
-    /**
-     * @var string config dir, relative to Environment.php
-     */
-    protected $configDir = '../../../config/';
-
-    /**
-     * @var string path to file that might override environment variable
-     */
-    protected $modeFile = '../../../config/mode.php';
 
     /**
      * Extend Environment class and merge parent array if you want to modify/extend these
