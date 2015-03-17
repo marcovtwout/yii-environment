80a6fa7f3fcc1259ef2e6cefa0edb87f5a1e96d6
marcovtwout <mr@vtwout.net>
2015-03-17 09:38:00 +0100

    DOC: set debug/trace also in test bootstrap and yiic

--- example-index/protected.tests.bootstrap.php
+++ example-index/protected.tests.bootstrap.php
@@ -5,6 +5,10 @@ require('../vendor/autoload.php');
 // set environment
 $env = new \marcovtwout\YiiEnvironment\Environment('TEST'); //override mode
 
+// set debug and trace level
+defined('YII_DEBUG') or define('YII_DEBUG', $env->yiiDebug);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', $env->yiiTraceLevel);
+
 // run Yii app
 require_once($env->yiitPath);
 require_once(dirname(__FILE__).'/WebTestCase.php');
--- example-index/protected.yiic.php
+++ example-index/protected.yiic.php
@@ -5,6 +5,10 @@ require('vendor/autoload.php');
 // set environment
 $env = new \marcovtwout\YiiEnvironment\Environment;
 
+// set debug and trace level
+defined('YII_DEBUG') or define('YII_DEBUG', $env->yiiDebug);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', $env->yiiTraceLevel);
+
 // run Yii app
 $config = $env->configConsole;
 require_once($env->yiicPath);
